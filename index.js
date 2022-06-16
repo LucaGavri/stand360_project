f1621ff9f3f067dd2dc106423af40976515c7bf6
Łukas Gawry <lukaszgawrys13@gmail.com>
2022-06-16 16:18:48 +0200

    v 0.1.3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const start = document.querySelector('.start');
+const body = document.querySelector('body');
 
+setTimeout(() => {
+    start.classList.add('maximize');
+}, 2000);
 
+setTimeout(() => {
+    start.classList.add('out');
+    body.style.overflow = 'auto'
+}, 3000);
